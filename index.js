dc65bd7c0a8b7f2bbae8a7a8191a8f3e45dc65a9
carlosduardo <carloseduardo@techie.com>
2024-11-21 13:03:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,12 +11,11 @@ let temperatura;
 function conversor() {
     if(paraFahrenheit.checked){
         temperatura = Number(caixaDeTexto.value);
-        resultado.textContent = temperatura * 1.8 + 32;
-        console.log(typeof temperatura)
+        resultado.textContent = (temperatura * 1.8 + 32) + "°F"       
     }
     else if(paraCelsius.checked){
         temperatura = Number(caixaDeTexto.value);
-        resultado.textContent = (5 * (temperatura - 32) / 9).toFixed(2);
+        resultado.textContent = ((5 * (temperatura - 32) / 9).toFixed(2)) + "°C";
     }
     else {
         resultado.textContent = "Selecione uma unidade de conversão!"
